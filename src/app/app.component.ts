06d7284e2d6d19fd412c861a2c54e349869ac0b5
mog-rn <mogaka.amo254@gmail.com>
2024-02-10 13:00:01 +0300

    feat(chore): latest chagnes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,43 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'freestyle';
+  nextSteps = [
+    {
+      heading: 'Heloo ',
+      description: 'Darkness my old friend. Darkness my old friend.Darkness my old friend.Darkness my old friend.Darkness my old friend.Darkness my old friend.Darkness my old friend.Darkness my old friend.',
+      title: 'Step 1',
+    },
+    {
+      heading: 'Heloo ',
+      description: 'Darkness my old friend',
+      title: 'Step 2',
+    },
+    {
+      heading: 'Heloo ',
+      description: 'Darkness my old friend',
+      title: 'Step 3',
+    },{
+      heading: 'Heloo ',
+      description: 'Darkness my old friend',
+      title: 'Step 3',
+    },{
+      heading: 'Heloo ',
+      description: 'Darkness my old friend',
+      title: 'Step 3',
+    },{
+      heading: 'Heloo ',
+      description: 'Darkness my old friend',
+      title: 'Step 3',
+    },
+    // {heading: 'Heloo ', description: 'Darkness my old friend'}
+    ];
 }
